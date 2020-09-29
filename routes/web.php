e9baeeb8eac71de8b27225fa3e3494a466a7760a
sethunathan <email.sethu@gmail.com>
2020-09-29 18:38:57 +0530

    git

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|git clone "git@github.xxxx.com:blablabla/reponame.git /Users/myname/dev/myfolder"
 */
 
 Route::get('/', function () {
@@ -22,7 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('git', function () {
     $data   = [];    
-    $data[] = shell_exec("git pull https://sethunathan::asdfghjkl12#$%^@github.com/sethunathan/kovamsam.git");   
+    $data[] = shell_exec("git pull https://sethunathan::asdfghjkl12!@#$^@github.com/wms-code/kovamsam.git");   
     dd($data);
     
 });
